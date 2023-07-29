6496fce854a7079d2061c04710f8600030cca5d3
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-07-29 22:54:51 +0900

    refactor: NODE_ENV is variable not secret

--- src/handler/type.ts
+++ src/handler/type.ts
@@ -2,13 +2,13 @@ type Env = {
   HISTORY: KVNamespace;
 
   // region wrangler.tomlに直接書き込まれている環境変数
+  NODE_ENV: 'production' | 'development';
   METER_DEVICE_ID: string;
   AIR_CONDITIONER_DEVICE_ID: string;
   SENTRY_DSN: string;
   // endregion
 
   // region GitHubの秘密変数に設定があって、GitHubActionsによって注入されている環境変数
-  NODE_ENV: 'production' | 'dev';
   SWITCHBOT_TOKEN: string;
   SWITCHBOT_CLIENT_SECRET: string;
   SENTRY_CLIENT_ID: string;
